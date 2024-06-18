1928af00c8ee7d206f3cc3f3dfb4c73fca8bf158
zackscovitch01 <bagoudoumoubarack@icloud.com>
2024-06-18 06:22:05 -0400

    .

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -2,8 +2,9 @@ import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import { useGlobalContext } from "./context";
 
-const URL =
-  "https://api.unsplash.com/search/photos?client_id=gnHrqHDGx4ndEnyy2y8mCCZRtiqCXg66dWxIjIDeUdk";
+const URL = `https://api.unsplash.com/search/photos?client_id=${
+  import.meta.env.VITE_API_KEY
+}`;
 
 const Gallery = () => {
   const { searchValue } = useGlobalContext();
